16f6e0e007a94e3ccc8fd479a3b8c6a2b2a432e8
huytag <huytagicloud@gmail.com>
2022-04-28 17:35:29 +0700

    fix UI code and fix header menu mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,24 +5,21 @@
       onCloseHamberger();
       onShowBtnSearchMobile();
       onClickSelect();
+      onClickShowMenuMobile();
     });
-
+    let clickHamber = false;
     function onShowHamberger() {
       const el_hamber = $("#button-hamberger");
-      let click = false;
       if (el_hamber.length > 0) {
         el_hamber.on("click", function (e) {
-          if (click) {
-            click = false;
-            $(".rn-header").removeClass("header-sticky-none");
+          if (clickHamber) {
+            clickHamber = false;
             $("#button-hamberger-nav").removeClass(
               "hamberger-menu-mobile_show"
             );
-          }
-          else {
-            click = true;
+          } else {
+            clickHamber = true;
             $("#button-hamberger-nav").addClass("hamberger-menu-mobile_show");
-            $(".rn-header").addClass("header-sticky-none");
           }
         });
       }
@@ -32,37 +29,36 @@
       const el_hamber = $("#close-hamberger-nav");
       if (el_hamber.length > 0) {
         el_hamber.on("click", function (e) {
+          clickHamber = false;
           $("#button-hamberger-nav").removeClass("hamberger-menu-mobile_show");
-          $(".rn-header").removeClass("header-sticky-none");
         });
       }
     }
-    function onShowBtnSearchMobile () {
-        const el_btn = $("#button-search");
-        let click = false;
-        if(el_btn.length > 0) {
-            el_btn.on("click",function (e) {
-                if(click) {
-                    click = false;
-                    $("#header-search").removeClass("is-active");
-                    $(".icon-search").removeClass("d-none");
-                    $(".icon-close-search").addClass("d-none");
-                }
-                else {
-                    click = true;
-                    $("#header-search").addClass("is-active");
-                    $(".icon-search").addClass("d-none");
-                    $(".icon-close-search").removeClass("d-none");
-                }
-            })
-        }
+    function onShowBtnSearchMobile() {
+      const el_btn = $("#button-search");
+      let click = false;
+      if (el_btn.length > 0) {
+        el_btn.on("click", function (e) {
+          if (click) {
+            click = false;
+            $("#header-search").removeClass("is-active");
+            $(".icon-search").removeClass("d-none");
+            $(".icon-close-search").addClass("d-none");
+          } else {
+            click = true;
+            $("#header-search").addClass("is-active");
+            $(".icon-search").addClass("d-none");
+            $(".icon-close-search").removeClass("d-none");
+          }
+        });
+      }
     }
     function onClickSelect() {
       const el_button = $(".nice-select");
       let click = false;
       if (el_button.length > 0) {
-        el_button.on("click",function (e) {
-          if(click) {
+        el_button.on("click", function (e) {
+          if (click) {
             click = false;
             $("#nice-select_active").removeClass("is-active");
             $("#icon-nice-select").removeClass("is-active");
@@ -71,8 +67,24 @@
             $("#nice-select_active").addClass("is-active");
             $("#icon-nice-select").addClass("is-active");
           }
+        });
+      }
+    }
+    function onClickShowMenuMobile() {
+      const el_menu = $("#mainmenu-mobile");
+      let click = true;
+      if(el_menu.length > 0) {
+        el_menu.on("click",function (e) {
+          if(click) {
+            click= false;
+            $("#submenu-mobile").removeClass("is-active")
+          } else {
+            click= true;
+            $("#submenu-mobile").addClass("is-active")
+          }
         })
       }
+      
     }
   });
 })(jQuery);
