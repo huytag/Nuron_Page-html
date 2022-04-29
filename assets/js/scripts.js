dd061c8d4cf9f0a19276000b6cf325df93aa68d7
huytag <huytagicloud@gmail.com>
2022-04-29 17:06:24 +0700

    add live bidding and countdown

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,10 @@
       onShowBtnSearchMobile();
       onClickSelect();
       onClickShowMenuMobile();
+      CountDownt();
+      liveSlickList();
+      SlickNext();
+      SlickPrex();
     });
     let clickHamber = false;
     function onShowHamberger() {
@@ -84,7 +88,80 @@
           }
         })
       }
-      
+    }
+    function CountDownt() {
+      let h = 9;
+      let m = 27;
+      let s = 60;
+      setInterval(() => {
+        s = s - 1;
+        if(s == 0) {
+          s = 60;
+          m = m - 1;
+          if (m == 0) {
+            m = 60;
+            h = h - 1;
+            if (h == 0) h = 12;
+          } 
+        };
+        $(".countdown-seconds").html(s);
+        $(".countdown-mins").html(m);
+        $(".countdown-hours").html("0"+h);
+      },1000)
+    }
+    function liveSlickList () {
+      $('.slick-list').not('.slick-initialized').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    }
+    function SlickNext() {
+      const el = $("#slick-list-btn-right");
+      if(el.length > 0) {
+        el.on("click", function (e) {
+          $('.slick-list').filter('.slick-initialized').slick('slickNext');
+        })
+      }
+    }
+    function SlickPrex() {
+      const el = $("#slick-list-btn-left");
+      if(el.length > 0) {
+        el.on("click", function (e) {
+          let number = $('.slick-list').slick('slickCurrentSlide');
+          if (number <= 0) return;
+          $('.slick-list').filter('.slick-initialized').slick('slickPrev');
+        })
+      }
     }
   });
 })(jQuery);
